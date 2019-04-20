e33b5fe29d6afd6b92340880e7c063967107fbd5
liuweilong96 <liuweilong0512@gmail.com>
2019-04-19 23:31:14 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+// window.onscroll = function() {myFunction()};
+// var nav = document.getElementByTagName("NAV")
+// var sticky = nav.offsetTop;
+// function myFunction() {
+//   if (window.pageYOffset >= sticky) {
+//     nav.classList.add("sticky")
+//   } else {
+//     nav.classList.remove("sticky");
+//   }
+// }
